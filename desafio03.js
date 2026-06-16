9ea0efba275610e775cc872ce2600642d933d89b
Soneca3 <pedrov.r.gonzaga@gmail.com>
2026-06-16 15:40:08 -0300

    Add files via upload

---
+++ desafio03.js
@@ -0,0 +1,9 @@
+let numero = Number(prompt("Digite um número"));
+if(numero %2 == 0){
+    console.log(`O número ${numero} é par`);
+    alert(`O número ${numero} é par`);
+}
+else{
+    console.log(`O número ${numero} é ímpar`);
+    alert(`O número ${numero} é ímpar`);
+}
