ba7b56f6d058d3edc19ba175e9adab09f592b464
Soneca3 <pedrov.r.gonzaga@gmail.com>
2026-06-16 15:23:23 -0300

    Add files via upload

---
+++ desafio01.js
@@ -0,0 +1,11 @@
+let numero = Number(prompt("Digite um número de 0 a 10"));
+
+if(numero > 0) {
+    console.log(`número ${numero} é positivo`);
+}
+else if(numero < 0){
+    console.log(`número ${numero} é negativo`);
+}
+else{
+    console.log(`número ${numero} é exatamente zero`)
+}
