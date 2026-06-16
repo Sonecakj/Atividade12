e41573e0424064bfb920514ec85f21f3cc0c7fb1
Soneca3 <pedrov.r.gonzaga@gmail.com>
2026-06-16 15:34:23 -0300

    desafio02.js

--- desafio02.js
+++ desafio02.js
@@ -1,9 +1,9 @@
 let idade = Number(prompt("Digite sua idade"));
 if(idade >= 18){
-    console.log(`Acesso liberado: Maior de idade`)
-    alert(`Acesso liberado: Maior de idade`)
+    console.log(`Acesso liberado: Maior de idade`);
+    alert(`Acesso liberado: Maior de idade`);
 }
 else{
-    console.log(`Acesso negado: Menor de idade`)
-    alert(`Acesso negado: Menor de idade`)
+    console.log(`Acesso negado: Menor de idade`);
+    alert(`Acesso negado: Menor de idade`);
 }
