6b258c8842935012e28460889b641e774ca529a5
Soneca3 <pedrov.r.gonzaga@gmail.com>
2026-06-16 16:38:52 -0300

    Add files via upload

---
+++ desafio08.js
@@ -0,0 +1,9 @@
+let numero = Number(prompt("Digite um valor"));
+if(numero >= 10 && numero<= 50){
+    console.log(`Está no invervalo permitido`);
+    alert(`Está no invervalo permitido`);
+}
+else{
+    console.log(`NÃO está no invervalo permitido`);
+    alert(`NÃO está no invervalo permitido`);
+}
